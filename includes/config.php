7ff90ea71aa88a811e85626b48b046636c306b2e
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-03-01 07:01:28 -0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,16 @@
-<?php
 
+// SQL server connection information
+<?php 
 
-$dbcon = mysqli_connect("localhost","artmir","Omeri1233","artmir") or die('enable connection'); 
-
-
+$sql_details = array(
+	'user' => 'xbase_leet',
+	'pass' => 'xbase_leet',
+	'db'   => 'xbase_leet',
+	'host' => 'localhost'
+);
 
 ?>
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP
+ * server-side, there is no need to edit below this line.
+ */
